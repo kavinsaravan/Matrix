7ea96eba0c2efd66fb5eee014873d5793b3aa8e9
Kavin Saravanan <kavinsaravan@gmail.com>
2023-03-17 19:20:34 -0700

    Initial commit

---
+++ src/ohio.java
@@ -0,0 +1,13 @@
+public class ohio {
+    public static int[][] createMatrix(int numberOfRows, int numberOfColumns) {
+        int[][] matrix = new int[numberOfRows][numberOfColumns];
+        int x = 0;
+        for (int i = 0; i < numberOfRows; i++) {
+            for (int j = 0; j < numberOfColumns; j++) {
+                matrix[i][j] = x;
+                x++;
+            }
+        }
+        return matrix;
+    }
+}
